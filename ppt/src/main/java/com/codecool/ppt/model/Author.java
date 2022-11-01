53739c6f15bcd941769f2492fab1b76c11d283b1
mariairinapreda <mariairina.predaeu@gmail.com>
2022-11-01 13:36:12 +0200

    created Author class

---
+++ ppt/src/main/java/com/codecool/ppt/model/Author.java
@@ -0,0 +1,16 @@
+package com.codecool.ppt.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+import java.util.List;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Document(collection = "authors")
+public class Author extends Person{
+    private List<Book> books;
+}
