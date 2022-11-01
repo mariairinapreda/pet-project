137d488a309bfeb5cf2a57bfb6bde2430f24bfab
mariairinapreda <mariairina.predaeu@gmail.com>
2022-11-01 13:36:49 +0200

    created AuthorController class

---
+++ ppt/src/main/java/com/codecool/ppt/controller/AuthorController.java
@@ -0,0 +1,24 @@
+package com.codecool.ppt.controller;
+
+import com.codecool.ppt.model.Book;
+import com.codecool.ppt.service.AuthorService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("author")
+public class AuthorController {
+    private final AuthorService authorService;
+
+    @Autowired
+    public AuthorController(AuthorService authorService) {
+        this.authorService = authorService;
+    }
+
+
+}
