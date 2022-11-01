790618e5f55aba59b35c8e10f9cf0d13a6872a27
mariairinapreda <mariairina.predaeu@gmail.com>
2022-11-01 13:39:25 +0200

    implemented BookController class

---
+++ ppt/src/main/java/com/codecool/ppt/controller/BookController.java
@@ -0,0 +1,37 @@
+package com.codecool.ppt.controller;
+
+import com.codecool.ppt.model.Author;
+import com.codecool.ppt.model.Book;
+import com.codecool.ppt.service.BookService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/books")
+public class BookController {
+    private final BookService bookService;
+
+    @Autowired
+    public BookController(BookService bookService) {
+        this.bookService = bookService;
+    }
+
+    @GetMapping
+    public List<Book> getAll() {
+        return bookService.getAllBooks();
+    }
+
+    @PostMapping
+    public void add(@RequestBody Book book) {
+        bookService.addBook(book);
+    }
+
+    @GetMapping("/{id}")
+    public Book getBook(@PathVariable("id") Long id) {
+        return bookService.getById(id).get();
+    }
+
+
+}
