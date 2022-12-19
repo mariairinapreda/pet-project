6c2f8df019cb73514babd5b676830b04ddeb892c
mariairinapreda <mariairina.predaeu@gmail.com>
2022-12-19 21:22:24 +0200

    implemented book page

---
+++ frontend/src/pages/Books/Books.tsx
@@ -0,0 +1,16 @@
+import React, { useEffect, useState } from "react";
+import {useAtom} from "jotai";
+import {DataAtom} from "../HomePage/components/bookData";
+import axios from "axios";
+import {CardComponent} from "../HomePage/components/CardComponent";
+
+const URL = 'http://localhost:8080/books';
+
+
+export  const Books = () => {
+    const [books, setBooks] = useAtom(DataAtom);
+    axios.get(URL).then(res => setBooks(res.data))
+  return(<div>
+      <CardComponent props={books}/>
+  </div>);
+}
