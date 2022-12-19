df9b1ef68037cb1461791d5fac6cd1b94e36ac89
mariairinapreda <mariairina.predaeu@gmail.com>
2022-12-19 21:26:50 +0200

    update index

--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -3,13 +3,23 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import {ChakraProvider} from "@chakra-ui/react";
+import { extendTheme } from '@chakra-ui/react'
 
+const theme = extendTheme({
+    fonts: {
+        heading: `'Lobster', sans-serif`,
+        body: `'Lobster', sans-serif`,
+    },
+})
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
 root.render(
   <React.StrictMode>
+      <ChakraProvider theme={theme}>
     <App />
+      </ChakraProvider>
   </React.StrictMode>
 );
 
