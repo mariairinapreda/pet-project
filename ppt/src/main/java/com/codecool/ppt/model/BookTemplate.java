5ad9a465556798bc14db83b371ec79bf7ba2a9d0
mariairinapreda <mariairina.predaeu@gmail.com>
2022-12-19 21:23:07 +0200

    created bookTemplate for adding books

---
+++ ppt/src/main/java/com/codecool/ppt/model/BookTemplate.java
@@ -0,0 +1,18 @@
+package com.codecool.ppt.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+public class BookTemplate {
+    private String name;
+    private String text;
+    private String author;
+    private String description;
+    private String imageUrl;
+}
