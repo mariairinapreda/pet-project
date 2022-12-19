ea2d4609f33b0d9e2d6f05646e361147ead09caf
mariairinapreda <mariairina.predaeu@gmail.com>
2022-12-19 21:30:02 +0200

    update carousel for homepage

---
+++ frontend/src/pages/HomePage/components/SliderContent.tsx
@@ -0,0 +1,25 @@
+import React, {FC} from "react";
+import {Box, Container} from "@chakra-ui/react";
+interface Values{
+    activeIndex: number,
+    sliderImage:{
+        urls: string
+    }[]
+}
+
+export const SliderContent : FC<Values> =({ activeIndex, sliderImage }) =>{
+    return (
+        <Box style={{width: "100%", height:"100%"}} >
+            {sliderImage.map((slide, index) => (
+                    <Box
+                        style={{width: "100%", height:"100%"}}
+                        key={index}
+                className={index === activeIndex ? "slides active" : "inactive"}
+        >
+        <img className="slide-image rounded-2xl" src={slide.urls} width={1000} height={800}  alt="" />
+        </Box>
+))}
+    </Box>
+);
+}
+
---
+++ frontend/src/pages/HomePage/components/SliderImage.tsx
@@ -0,0 +1,15 @@
+export default [
+    {
+        urls: "https://user-images.githubusercontent.com/89586309/203042781-df6ece55-b933-48cd-94a8-fcc107981c14.png",
+    },
+    {
+        urls: "https://user-images.githubusercontent.com/89586309/203042781-df6ece55-b933-48cd-94a8-fcc107981c14.png",
+    },
+    {
+        urls: "https://user-images.githubusercontent.com/89586309/203042781-df6ece55-b933-48cd-94a8-fcc107981c14.png",
+    }]
+
+
+//"https://user-images.githubusercontent.com/89586309/200557699-e81d843a-abc4-4d89-9994-900b007717b0.png"
+//"https://user-images.githubusercontent.com/89586309/200557445-6f2e421a-8452-41cc-ac34-cd3acbe18c2f.png"
+//"https://user-images.githubusercontent.com/89586309/200558028-ba0d37c4-3bdc-426d-bfb3-5faae24bcd40.png"
