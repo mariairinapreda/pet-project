4f8c4f2ce60c76a6ccf4f9a224f0c1d5f1a08a60
mariairinapreda <mariairina.predaeu@gmail.com>
2022-12-19 21:19:47 +0200

    created author component

---
+++ frontend/src/pages/Authors/Authors.tsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import {useAtom} from "jotai";
+import {DataAtom} from "../HomePage/components/bookData";
+import axios from "axios";
+const URL = 'http://localhost:8080/authors';
+const Authors = ()=> {
+    const [authors, setAuthors] = useAtom(DataAtom);
+    axios.get(URL).then(res => setAuthors(res.data))
+        return (
+            <div>
+
+            </div>
+        );
+
+}
+
+export default Authors;
