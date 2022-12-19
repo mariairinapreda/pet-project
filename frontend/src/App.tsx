b5fb7f9e6a69ae93b2485bd1710a7553adfe319d
mariairinapreda <mariairina.predaeu@gmail.com>
2022-12-19 21:15:56 +0200

    update App

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,25 +1,32 @@
 import * as React from 'react'
-import { HashRouter as Router, Route,Routes } from "react-router-dom"
+import { BrowserRouter as Router, Route,Routes } from "react-router-dom"
 import {Header} from "./constants/header/Header"
 import {Footer} from "./constants/footer/Footer";
 import {HomePage} from "./pages/HomePage/components/HomePage";
+import "./App.css"
+import SignIn from "./pages/SignIn/SignIn";
+import Register from "./pages/Register/Register";
+import {Books} from "./pages/Books/Books";
+import AddBook from "./pages/addBook/AddBook";
+
 
 class App extends React.Component {
   render() {
     return (
         <Router>
-            <div className="App">
+            <div className="App text-center sm:text-left">
                 <Header/>
-                <HomePage/>
-                <Footer/>
             </div>
             <Routes>
-                <Route path={"/"} />
-                <Route  path={"/books"}/>
+                <Route path={"/"} element={<HomePage/>} />
+                <Route  path={"/books"} element={<Books/>}/>
                 <Route  path={"/authors"} />
-                <Route path={"/books/add"} />
+                <Route path={"/books/add"} element={<AddBook/>} />
                 <Route  path={"/authors/add"} />
+                <Route  path={"/signin"} element={<SignIn/>} />
+                <Route  path="/register" element={<Register/>} />
             </Routes>
+            <Footer/>
         </Router>
 
     );
