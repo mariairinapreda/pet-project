78e883467baf2a6d1cec83ad8754a9edad55d0ef
mariairinapreda <mariairina.predaeu@gmail.com>
2023-01-07 20:15:56 +0200

    cors configuration

--- ppt/src/main/java/com/codecool/ppt/corsConfig/WebConfig.java
+++ ppt/src/main/java/com/codecool/ppt/corsConfig/WebConfig.java
@@ -1,4 +1,4 @@
-package com.codecool.ppt;
+package com.codecool.ppt.corsConfig;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
