8519ea41790b52417099e32c519e405e44127b0c
mariairinapreda <mariairina.predaeu@gmail.com>
2023-01-08 16:34:22 +0200

    improved design to read book page

--- frontend/src/pages/readBook/ReadBookPage.tsx
+++ frontend/src/pages/readBook/ReadBookPage.tsx
@@ -4,19 +4,36 @@ import axios from "axios";
 import {useParams} from "react-router-dom";
 import {infoContentAtom} from "../addBook/infoContentAtom";
 import {Document, Page} from "react-pdf/dist/esm/entry.webpack"
-import {Button} from "@chakra-ui/react";
+import {Button, textDecoration} from "@chakra-ui/react";
 import "./ReadBook.css"
 
 
+
 const ReadBookPage =()=> {
-const [numPage, setNumPages] = useState<number>(227);
+    let { name } = useParams<{ name: string}>();
+    const [book, setBook]=useAtom(infoContentAtom);
+    const [numberOfPages, setNumberOfPages] = useState<number>(0);
+
+    axios.get(`http://localhost:8080/api/content/${name}`).then(res =>setBook(res.data))
+    axios.get(`http://localhost:8080/api/books/${name}`).then(res =>setNumberOfPages(res.data))
+const [numPage, setNumPages] = useState<number>(0);
 const [pageNumber, setPageNumber] = useState(1);
-function onDocumentLoadSuccess(numPages: number){
-setNumPages(numPages);
-setPageNumber(1);
-}
+
 function changePage(offset: number){
     setPageNumber(prevState => prevState + offset);
+}
+function opacifyNavbar() {
+    const footer = document.getElementById("footer");
+    if (footer !== null) {
+        footer.style.opacity = "0.3";
+    }
+    const list = new Array(document.querySelector("nav"));
+    list.forEach(n => {
+        if (n !== null) {
+            n.style.opacity = "0.3";
+        }
+    })
+
 }
 function changePageBack(){
     changePage(-1);
@@ -24,28 +41,27 @@ function changePageBack(){
 function changePageNext(){
     changePage(1);
 }
-    let { name } = useParams<{ name: string}>();
-    const [book, setBook]=useAtom(infoContentAtom);
-
-    axios.get(`http://localhost:8080/api/books/${name}`).then(res =>setBook(res.data))
     const b64toBlob = require('b64-to-blob');
 
     const contentType = 'application/pdf';
 
     const blob = b64toBlob(book, contentType);
     const objectUrl = window.URL.createObjectURL(blob);
-
         return (
             <div id={"pdf"}>
-                <Document file={objectUrl}   renderMode={"canvas"} onLoadSuccess={()=>onDocumentLoadSuccess}>
-                    <Page renderTextLayer={false} renderAnnotationLayer={false}  renderMode={"canvas"}  height={600} pageNumber={pageNumber}/>
+
+                <Document file={objectUrl}   renderMode={"canvas"} onLoadSuccess={()=>{
+                    setNumPages(numberOfPages);
+                    opacifyNavbar();
+                }}>
+                    <Page renderTextLayer={false}  renderAnnotationLayer={false}  renderMode={"canvas"}  height={800} pageNumber={pageNumber}/>
                 </Document>
                 <p> Page number {pageNumber} of {numPage}</p>
                 {pageNumber > 1 &&
-                    <Button id={"left"} onClick={changePageBack}>Previous</Button>}
+                    <span id={"left"} onClick={changePageBack}>&#10094;</span>}
                 {pageNumber < numPage &&
 
-                    <Button id={"right"} onClick={changePageNext}>Next</Button>}
+                    <span id={"right"} onClick={changePageNext}>&#10095;</span>}
             </div>
         );
 
