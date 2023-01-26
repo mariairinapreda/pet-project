9e8a645b6f517729ccca3dd71ba16e6e5a17fd5c
mariairinapreda <mariairina.predaeu@gmail.com>
2023-01-26 14:37:49 +0200

    updated Reader service

---
+++ ppt/src/main/java/com/codecool/ppt/service/ReaderService.java
@@ -0,0 +1,33 @@
+package com.codecool.ppt.service;
+
+import com.codecool.ppt.model.Reader;
+import com.codecool.ppt.repository.ReaderRepository;
+import lombok.AllArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@AllArgsConstructor
+@Service
+public class ReaderService {
+private final ReaderRepository repository;
+
+public Optional<Reader> getByMail(String email){
+    return repository.findByEmail(email);
+}
+public void addReader(Reader reader)
+{
+    repository.save(reader);
+}
+
+public List<Reader> getAllReaders(){
+    return repository.findAll();
+}
+public Reader getById(String id){
+    return repository.findById(id).get();
+}
+
+
+
+}
