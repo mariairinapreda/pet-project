9b41757df4b70634af25dd3facc9ee8265a8ed11
mariairinapreda <mariairina.predaeu@gmail.com>
2023-01-26 14:36:36 +0200

    updated Reader class

--- ppt/src/main/java/com/codecool/ppt/model/Reader.java
+++ ppt/src/main/java/com/codecool/ppt/model/Reader.java
@@ -1,14 +1,62 @@
 package com.codecool.ppt.model;
 
-import lombok.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.Collection;
+import java.util.List;
 
 @Setter
 @Getter
+@Builder
 @Document
 @AllArgsConstructor
 @NoArgsConstructor
-public class Reader extends Person{
+public class Reader implements UserDetails {
+    @Id
+    protected String id;
+    protected String firstName;
+    protected String lastName;
     private String email;
-    private Boolean isPremium;
+    private String password;
+    private Role role;
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return List.of(new SimpleGrantedAuthority(role.name()));
+    }
+
+    @Override
+    public String getUsername() {
+        return email;
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return true;
+    }
+    //    private Boolean isPremium;
 }
