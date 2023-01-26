8c590e87916897fa8d2e16bd0bde1f19042a3cb5
mariairinapreda <mariairina.predaeu@gmail.com>
2023-01-26 14:33:33 +0200

    added useState to form for storing input

--- frontend/src/constants/header/Forms.tsx
+++ frontend/src/constants/header/Forms.tsx
@@ -1,16 +1,24 @@
-import React, {Component} from 'react';
+import React, {Component, useState} from 'react';
 import {FormControl, FormHelperText, Input} from "@chakra-ui/react"
-class Forms extends Component {
-    render() {
+import axios from "axios";
+import {useAtom} from "jotai";
+import {getEmptySearch} from "../../types/SearchInput";
+import {DataAtom} from "../../pages/HomePage/components/bookData";
+const Forms =()=>{
+    const [search, setSearch] = useState(getEmptySearch);
+    const[input , setInput] = useAtom(DataAtom);
+    // axios.get(`http://localhost:8080/api/books/filter/${search}`, {
+    //     headers: {
+    //         "Access-Control-Allow-Origin":`http://localhost:8080/api/books/filter/${search}`
+    //     }
+    // }).then(res => setInput(res.data))
         return (
             <div>
                 <FormControl>
-                    <Input type='search'  className={"text-white"} focusBorderColor={"#A84839"} placeholder={"search"} />
+                    <Input type='search' onChange={(e)=>setSearch(prevState =>({ ...prevState,"input": e.target.value}))} className={"text-white"} focusBorderColor={"#A84839"} placeholder={"search"} />
                     <FormHelperText fontFamily={"Lobster"}>We have all the answers</FormHelperText>
                 </FormControl>
             </div>
         );
     }
-}
-
 export default Forms;
