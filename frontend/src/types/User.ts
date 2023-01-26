432c454c8f43ca26d016b3a66386e6d92ebfe9b4
mariairinapreda <mariairina.predaeu@gmail.com>
2023-01-26 14:42:10 +0200

    created type for user

---
+++ frontend/src/types/User.ts
@@ -0,0 +1,15 @@
+export type User ={
+    token : any,
+    id :any,
+    name:any,
+    role:any
+}
+
+export function getEmptyUser() : User{
+    return {
+        token : null,
+        id :null,
+        name:null,
+        role:null
+    }
+}
