e9abd92b6bde65ee845f82507a1be35f306ef372
mariairinapreda <mariairina.predaeu@gmail.com>
2023-01-26 14:25:41 +0200

    implemented controller for AuthenticationRequest object

---
+++ ppt/src/main/java/com/codecool/ppt/model/AuthenticationRequest.java
@@ -0,0 +1,21 @@
+package com.codecool.ppt.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+@Setter
+@Getter
+@Builder
+@Document
+@AllArgsConstructor
+@NoArgsConstructor
+public class AuthenticationRequest {
+private String email;
+private String password;
+
+
+}
