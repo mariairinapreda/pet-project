da3db6bf66e98f8ccd0924913ff412654e1e11ee
mariairinapreda <mariairina.predaeu@gmail.com>
2023-01-26 14:30:43 +0200

    added query for search

--- ppt/src/main/java/com/codecool/ppt/repository/BookRepository.java
+++ ppt/src/main/java/com/codecool/ppt/repository/BookRepository.java
@@ -3,12 +3,14 @@ package com.codecool.ppt.repository;
 import com.codecool.ppt.model.Author;
 import com.codecool.ppt.model.Book;
 import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.data.mongodb.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
-import java.util.UUID;
 @Repository
 public interface BookRepository extends MongoRepository<Book, String> {
+    @Query("{author: ?0, cost: ?1}")
+    List<Book> findBooksBySearch(String search);
     List<Book> findBooksByAuthor(Author author);
     Book findBookByNameEquals(String name);
     boolean existsBookByNameEquals(String name);
